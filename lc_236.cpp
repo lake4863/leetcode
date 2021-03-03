1f5a6bb9f13bddf5c7094e9dd1ce1f0288309e7e
Chane Li <qli427@gatech.edu>
2021-03-02 21:38:25 -0800

    remove aux function

--- lc_236.cpp
+++ lc_236.cpp
@@ -11,30 +11,13 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root) {
-            bool lp = isAncestor(root->left, p);
-            bool lq = isAncestor(root->left, q);
-            bool rp = isAncestor(root->right, p);
-            bool rq = isAncestor(root->right, q);
-            
-            if(lp && lq) {
-                return lowestCommonAncestor(root->left, p, q);
-            } 
-	    if(rp && rq) {
-                return lowestCommonAncestor(root->right, p, q);
-            }
-        }
+        if(!root || p == root || q == root) return root;
+        
+        TreeNode* lhs = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rhs = lowestCommonAncestor(root->right, p, q);
+        
+        if(!lhs) return rhs;
+        if(!rhs) return lhs;
         return root;
-    }
-    
-    bool isAncestor(TreeNode* parent, TreeNode* p) {
-        if(!parent) return false;
-        bool isleft = isAncestor(parent->left, p);
-        bool isright = isAncestor(parent->right, p);
-            
-        if(parent != p) {
-            return isleft || isright;
-        }
-        return true;
     }
 };
