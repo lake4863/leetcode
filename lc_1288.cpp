fa14e92639f271a67102f54c53988b9a9cbb3d78
Chane Li <qli427@gatech.edu>
2021-03-15 21:50:11 -0700

    need optimize sorting part

---
+++ lc_1288.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        if(intervals.size() < 2) return 0;
+        int result = 0;
+        vector<int> pre;
+        
+        sort(intervals.begin(), intervals.end(), [](auto lhs, auto rhs) {
+            if(lhs[1] != rhs[1]) {
+                return lhs[1] < rhs[1];
+            } else {
+                return lhs[0] > rhs[0];
+            }
+        });
+        
+        //for(auto i : intervals) cout << i[0] << " : " << i[1] << endl;
+        
+        pre = intervals[intervals.size() - 1];
+        for(int i = intervals.size() - 2; i >= 0; --i) {
+            //cout << intervals[i][0] << ", " << intervals[i][1] << endl;
+            if(intervals[i][0] >= pre[0]) {
+            //cout << intervals[i][0] << ",** " << intervals[i][1] << endl;
+                ++result;
+            } else if(intervals[i][0] <= pre[0]) {    // && intervals[i][1] >= pre[1]) {
+                pre = intervals[i];
+            }
+        }
+        
+        return intervals.size() - result;
+    }
+};
