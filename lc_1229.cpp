1f2fd4f81164373cba277644cc71c59d75ae5a59
Chane Li <qli427@gatech.edu>
2021-03-18 13:17:05 -0700

    updated after using priority_queue

--- lc_1229.cpp
+++ lc_1229.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
+        for(auto i : slots1) {
+            if(i[1] - i[0] >= duration) {
+                pq.push({ i[0], i[1] });
+            }
+        }
+        
+        for(auto i : slots2) {
+            if(i[1] - i[0] >= duration) {
+                pq.push({ i[0], i[1] });
+            }
+        }
+        
+        while(pq.size() > 1) {
+            //cout << pq.top().first << ", " << pq.top().second << endl;
+            int end = pq.top().second;
+            pq.pop();
+            int start = pq.top().first;
+            if(start <= end - duration) {
+                return { start, start + duration };
+            }
+        }
+        
+        return {};
+    }
+};
+
+class Solution {
+public:
+    vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
         map<int, int> m;
         vector<int> result = { -1, -1 };
         int count = 0;
