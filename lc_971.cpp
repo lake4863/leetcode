36e8ca2afdde27971e448ad11ee80e0d3b03903e
Chane Li <qli427@gatech.edu>
2021-04-09 18:58:29 -0700

    update an easier undstood version

--- lc_971.cpp
+++ lc_971.cpp
@@ -1,5 +1,29 @@
 class Solution {
 private:
+    vector<int> nodes;
+    int i = 0;
+public:
+    vector<int> flipMatchVoyage(TreeNode* root, vector<int>& voyage) {
+        return traverse(root, voyage) ? nodes : vector<int>{ -1 };
+    }
+    
+    bool traverse(TreeNode* root, vector<int>& v) {
+        if(!root) return true;
+        
+        if(root->val != v[i++]) return false;
+        if(root->left && root->left->val != v[i]) {
+            nodes.push_back(root->val);
+            if(root->right) {
+                swap(root->left, root->right);
+            }
+        }
+        
+        return traverse(root->left, v) && traverse(root->right, v);
+    }
+};
+
+class Solution {
+private:
 	vector<int> res;
     int i = 0;
 public:
