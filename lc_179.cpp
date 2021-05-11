a5b20522bc91d750f2a8500148275c7859341809
Chane Li <qli427@gatech.edu>
2021-05-11 08:12:28 -0700

    haven't pass yet

---
+++ lc_179.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<string> vs(n);
+        for(int i = 0; i < n; ++i) {
+            vs[i] = to_string(nums[i]);
+        }
+        
+        sort(vs.begin(), vs.end(), [](string lhs, string rhs) {
+            int l = lhs.length();
+            int r = rhs.length();
+            
+            if(l == r) {
+                return lhs < rhs;
+            } else {
+                int len = min(l, r);
+                for(int i = 0; i < len; ++i) {
+                    if(lhs[i] > rhs[i]) {
+                        //cout << "hi hi" << endl;
+                        return lhs < rhs;
+                    } else {
+                        //cout << "hi" << endl;
+                        return rhs < lhs;
+                    }
+                }
+                return (l > r) ? ((lhs[len] >= lhs[0]) ? lhs > rhs : rhs > lhs) : ((rhs[len] >= rhs[0]) ? lhs < rhs : rhs < lhs);
+            }
+        });
+        //for(auto i : vs) cout << i << " ";
+        string result;
+        
+        for(int i = n - 1; i > -1; --i) {
+            result += vs[i];
+        }
+        
+        return result; 
+    }
+};
