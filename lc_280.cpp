cab33241df230dfd359a000ac079e95625560e79
Chane <chane@Chanes-MacBook-Air.local>
2021-08-20 10:03:09 -0700

    20210819

---
+++ lc_280.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        bool less = true;
+        for(int i = 0; i < nums.size() - 1; ++i) {
+            if(less) {
+                if(nums[i] > nums[i + 1]) {
+                    swap(nums[i], nums[i + 1]);
+                }
+            } else {
+                if(nums[i] < nums[i + 1]) {
+                    swap(nums[i], nums[i + 1]);
+                }
+            }
+            less = !less;
+        }
+    }
+};
